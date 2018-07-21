1ddf466948239590450283c01e4ccd6a6e4218b5
Vikky Ambarkar <ytubeindia2@gmail.com>
2018-07-21 13:21:44 +0530

    added babel transform class properties

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import perdata from "../../data.json";
 
-const Search = () => (
-    <div className="search">
+class Search extends Component {
+    state = {
+      searchTerm: ''
+    }
+    handleSearchTermChange = (event) => {
+        this.setState({searchTerm: event.target.value})
+    }
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input type="text" placeholder="Search" onChange = {this.handleSearchTermChange} value={this.state.searchTerm}  />
+        </header>
         <div>
-            {perdata.shows.map(show => <ShowCard key={show.imdbID} {...show}/>)}
+          {perdata.shows.filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
-    </div>
-);
+      </div>
+    );
+  }
+}
 
 export default Search;
